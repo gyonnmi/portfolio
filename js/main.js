91825f54928343dd460916b2c7c0125f8b33e47d
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-09-14 01:37:12 +0900

    '0914ArrowUp'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* main.js */
 'use strict';
 
+// 반응형 토글 버튼
 const toggleBtn = document.querySelector('.toggle-btn');
 const navbar = document.querySelector('.navbar');
 let navActive = false;
@@ -13,6 +14,7 @@ toggleBtn.addEventListener('click', ()=> {
   }
 });
 
+// Nav 스크롤 이동
 navbar.addEventListener('click', ()=> {
   const target = event.target;
   const link = target.dataset.link;
@@ -20,6 +22,7 @@ navbar.addEventListener('click', ()=> {
   document.querySelector(link).scrollIntoView();
 });
 
+// Nav Scroll Fixed
 const nav = document.querySelector('.nav');
 const navHeight = nav.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=> {
@@ -30,10 +33,32 @@ document.addEventListener('scroll', ()=> {
   }
 })
 
+// Contact Btn
 const contactBtn = document.querySelector('.home-contact');
 contactBtn.addEventListener('click', ()=> {
   const contact = document.querySelector('#contact');
   contact.scrollIntoView();
+});
+
+// Home Scroll Opacity
+const home = document.querySelector('.home-inner-container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show Arrow up
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+arrowUp.addEventListener('click', ()=> {
+  const home = document.querySelector('.home-container');
+  home.scrollIntoView();
 });
 
 
