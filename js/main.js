6cbfca24a39751d5269cdf7feddfbdf6ed6642af
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-09-13 22:10:16 +0900

    '0913Footer'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* main.js */
+'use strict';
 
 const toggleBtn = document.querySelector('.toggle-btn');
 const navbar = document.querySelector('.navbar');
@@ -12,3 +13,12 @@ toggleBtn.addEventListener('click', ()=> {
   }
 });
 
+const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+  if(window.scrollY > navHeight) {
+    nav.classList.add('nav-dark');
+  } else {
+    nav.classList.remove('nav-dark');
+  }
+})
